c2fba1ec57231c3c744938d7e81d970738f6b7db
Padout <130877761+Padout@users.noreply.github.com>
2025-11-02 08:20:23 -0500

    Add documents page

---
+++ pages/documents.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+
+const Documents: NextPage = () => {
+  return (
+    <main className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Documents</h1>
+      <p>This page will allow upload and viewing of documents.</p>
+    </main>
+  );
+};
+
+export default Documents;
